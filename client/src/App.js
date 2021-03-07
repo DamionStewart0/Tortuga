b6c49810abb6dff9a0e879528a81dc52bcce9743
Developer genius <developer_genius@protonmail.com>
2021-03-07 18:58:38 -0500

    Routes completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import { Layout } from './layouts/Layout';
 
 function App() {
   return (
     <Layout>
-      <h3>Welcome to Tortuga</h3>
+      <Switch>
+        <Route path='/login'>
+          <h3>Log-in to Tortuga</h3>
+        </Route>
+
+        <Route path='/register'>
+          <h3>Register for entry</h3>
+
+        </Route>
+
+        <Route path='/Reviews'>
+          <h3> Enter</h3>
+
+        </Route>
+      </Switch>
     </Layout>
   );
 }
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 
+
 export const Layout = (props) => {
     return (
         <div className ='App'>
         <header>
             <h1>Tortuga</h1>
-            <Link>Login/Register</Link>
-            <Link>Review</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/reviews'>Review</Link>
         </header>
         {props.children}
 
