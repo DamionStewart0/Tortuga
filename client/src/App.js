d8f3d7cb761d3f8cbd543e43abeae3ccdd8dd91e
Developer genius <developer_genius@protonmail.com>
2021-03-08 14:45:05 -0500

    -damion to ds-dame-setup -Complete the Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+import {Switch, Route, useHistory} from 'react-router-dom';
 import { Layout } from './layouts/Layout';
 import { Login } from './screens/Login';
+import {loginUser, registerUser} from './services/auth'
+import { Register } from './screens/Register';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  const history = useHistory();
+
+  const handleLogin = async (formData) => {
+    const currentUser = await loginUser(formData);
+    setCurrentUser(currentUser)
+    history.push('/')
+  }
+  const handleRegister = async (formData) => {
+    const currentUser = await registerUser(formData);
+    setCurrentUser(currentUser)
+    history.push('/')
+  }
   return (
     <Layout>
       <Switch>
         <Route path='/login'>
-          <Login />
+          <Login  handleLogin={handleLogin}/>
         </Route>
 
         <Route path='/register'>
-          <h3>Register for entry</h3>
+          <Register handleRegister={handleRegister} />
 
         </Route>
 
         <Route path='/review'>
           <h3>Enter</h3>
 
+        </Route>
+        <Route path='/'>
+          <h3>Home</h3>
         </Route>
       </Switch>
     </Layout>
